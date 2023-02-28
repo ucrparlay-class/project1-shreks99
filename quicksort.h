4efd2c6929348978c9093dc1ecf705d6462fd7d5
ssinh017 <shreyk1999@gmail.com>
2023-02-28 13:05:58 -0800

    Added quicksort with coaserning

--- quicksort.h
+++ quicksort.h
@@ -45,7 +45,24 @@ void scan_down(T* A, T* B, T* ls, size_t n, size_t offset) {
         [&] { scan_down(A,B,ls,m,offset);},
         [&] { scan_down(A + m, B+m, ls + m, n - m,offset + ls[m-1]);});
   }
-
+//For calcuating the leftsum in sequential for prefix-sum
+template <class T>
+T seq_scan_up(T *A, T *ls, size_t n) {
+  T sum = 0;
+  for(size_t i=0;i<n;i++)
+    sum+=A[i];
+  return sum;
+}
+//For Calulating prefix sum in sequential
+template <class T>
+void seq_scan(T *A, T *B, size_t n) {
+   B[0]=A[0];
+    for(size_t i=1;i<n;i++) {
+      A[i] = A[i-1] + A[i];
+      B[i] = A[i];
+    }
+    return;
+}
 //For calculating the leftsum for prefix-sum
 template <class T>
 T scan_up(T *A, T *ls, size_t n) {
@@ -64,11 +81,7 @@ T scan_up(T *A, T *ls, size_t n) {
 template <class T>
 void pscan(T *A, T *B,size_t n) {
   if(n<THRESHOLD) {
-    for(size_t i=1;i<n;i++) {
-      A[i] = A[i-1] + A[i];
-      B[i] = A[i];
-    }
-    return;
+    seq_scan(A,B,n);
   }
     T* ls = (T*)malloc(n * sizeof(T));
     scan_up(A, ls, n);
@@ -77,39 +90,46 @@ void pscan(T *A, T *B,size_t n) {
 }
 
 template <class T,class Func>
-size_t filter(T* A, size_t n, T* B, size_t pivot,const Func& f) {
+T filter(T* A, size_t n, T* B, T pivot,const Func& f) {
     T* flag = (T*)malloc(n * sizeof(T));
     T* ps = (T*)malloc(n * sizeof(T));
-
+    if(n<THRESHOLD){
+      for(size_t i=0;i<n;i++) flag[i] = f(A[i],pivot);
+      seq_scan(flag,ps,n);
+      for(size_t i=0;i<n;i++) if(f(A[i],pivot)) {
+          B[ps[i]-1] = A[i]; 
+        }
+      return ps[n-1];    
+    }
     parallel_for(0, n, [&](size_t i) {
         flag[i] = f(A[i],pivot);
     });
+     
     pscan(flag,ps,n);
     parallel_for(0, n, [&](size_t i) {
         if(f(A[i],pivot)) {
           B[ps[i]-1] = A[i]; 
         }
-    }
-    );
+    });
+    
   return ps[n-1];    
 }
 
 template <class T>
 T para_partition(T *A, size_t n, T pivot) {
+ 
   T* B = (T*)malloc(n * sizeof(T)); 
   auto f_less = [](T value, T pivot) { return pivot>value; };
   auto f_more = [](T value, T pivot) { return pivot<value; };
   auto f_equal = [](T value, T pivot) { return pivot==value; };
 //less values
-  size_t count = filter(A,n,B,pivot,f_less);
-
+  T count = filter(A,n,B,pivot,f_less);
 //equal values
-  count += filter(A,n,B+count,pivot,f_equal);
-
+  T count2  = count + filter(A,n,B+count,pivot,f_equal);
 //large
-  filter(A,n,B+count,pivot,f_more);
-      
-  return count;
+  filter(A,n,B+count2,pivot,f_more);
+  parallel_for(0, n, [&](size_t i) {A[i] = B[i];});
+  return count2;
 }
 
 template <class T>
@@ -118,31 +138,20 @@ T custom_rand(T* A,size_t n,size_t threshold) {
   for(size_t i=0;i<threshold;i++)
     index[i] = A[hash64_(i)%(n)];
   std::sort(index,index+threshold);
-  
   return index[threshold/2];
 }
 
 template <class T>
 void quicksort(T *A, size_t n) {
+  if(n<=1) return; 
   if(n<THRESHOLD){
-    T pivot = hash64_(std::time(nullptr))%n;
-    seq_partition(A,n,pivot);
-  }  else
-    para_partition(A,n,custom_rand(A,n,THRESHOLD));
-
+    std::sort(A,A+n);
+  } else{
+  T t = para_partition(A,n,custom_rand(A,n,THRESHOLD));
+    if((size_t)t < n){
+      parlay::par_do(
+            [&] { quicksort(A,t);},
+            [&] { quicksort(A+t,n-t);});
+    }
+  }
 }
-
-// template <class T>
-// void qsort(T *A, size_t start,size_t end) {
-//  // T j;
-//   //  if(n<threshold) {
-//   //   j = seq_partition(A+start,start,end);
-//   // }
-//   T  j = para_partition(A+start,end-start+1,A[start+hash64_(std::time(nullptr))%(end-start+1)]);
-
-    
-//     par_do(
-//         [&] { qsort(A,start, j); },
-//        [&] { qsort(A+start,start + j, end); });
-
-// }
